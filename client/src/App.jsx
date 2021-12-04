f1d4511aca243ef7586c790c22796419e621b6c3
saja-aabith <saja.aabith@gmail.com>
2021-12-04 22:53:46 +0000

    add markers basic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './style.css';
+import ReactMapGL, { Marker } from 'react-map-gl';
 
-// import { NavBar } from './components/NavBar/index';
 
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -19,11 +19,37 @@ import Menu from '@mui/material/Menu';
 import mapboxgl from '!mapbox-gl';
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2FqYTM2IiwiYSI6ImNrd3JtMWtzazBpM2syb285dTN4dWNyd2sifQ.L5VJBCeE8JNppDI41T7CpQ';
 
+const mapStyle = {
+    width: '100%',
+    height: 600
+}
+
+const mapboxApiKey = 'pk.eyJ1Ijoic2FqYTM2IiwiYSI6ImNrd3JtMWtzazBpM2syb285dTN4dWNyd2sifQ.L5VJBCeE8JNppDI41T7CpQ';
 
 export const App = () => {
 
     const [auth, setAuth] = React.useState(true);
     const [anchorEl, setAnchorEl] = React.useState(null);
+    const [viewport, setViewport] = React.useState(
+        {
+            latitude: 51.5072,
+            longitude: 0.1276,
+            zoom: 10
+        });
+    const [markers, setMarkers] = React.useState(
+        [{
+            latitude: 51.5072,
+            longitude: 0.1276,
+            name: 'test-1'
+        }, {
+            latitude: 56.5072,
+            longitude: 5.1276,
+            name: 'test-2'
+        }, {
+            latitude: 46.5072,
+            longitude: -10.1276,
+            name: 'test-3'
+        },]);
 
     const handleChange = (event) => {
         setAuth(event.target.checked);
@@ -37,38 +63,27 @@ export const App = () => {
         setAnchorEl(null);
     };
 
-    // mapbox
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(0.1276);
-    const [lat, setLat] = useState(51.5072);
-    const [zoom, setZoom] = useState(6);
 
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [lng, lat],
-            zoom: zoom
-        });
-    });
-
-    //mapbox storing new coordinates
-    useEffect(() => {
-        if (!map.current) return; //waits for the map to initialise
-        map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));
-        });
-    });
+    const onSelected = (item) => {
+        setMarker({
+            name: item.place_name,
+            longitude: item.center[0],
+            latitude: item.center[1]
+        })
+    }
 
 
 
-
-
-
+    let markerCollection = [];
+    markers.forEach((marker, index) => {
+        markerCollection.push(
+            <Marker
+                longitude={marker.longitude}
+                latitude={marker.latitude}>
+                <div className="marker temporary-marker"><span></span></div>
+            </Marker>
+        )
+    })
 
     return (
         <>
@@ -135,12 +150,21 @@ export const App = () => {
                 </AppBar>
             </Box>
             <br></br>
-            <div>
-                <div className="sidebar">
-                    Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-                </div>
-                <div ref={mapContainer} className="map-container" />
-            </div>
+            <ReactMapGL
+                mapboxApiAccessToken={mapboxApiKey}
+                mapStyle="mapbox://styles/mapbox/streets-v11"
+                {...viewport}
+                {...mapStyle}
+                onViewportChange={(viewport) => setViewport(viewport)}
+            >
+                {markerCollection}
+
+            </ReactMapGL>
         </>
     );
 }
+
+// 1. Currently the marker is an object. Convert that into an array of objects
+// 2. Use a for loop to display all markers
+// 3. Change color of marker to red
+// 4.  
