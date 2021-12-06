07a793076b098dfcdcdc8b71b20f04df17e8a0ca
olu-akano <akanopaul@yahoo.co.uk>
2021-12-06 15:08:53 +0000

    add review views

--- server/travelado/reviews/views.py
+++ server/travelado/reviews/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Reviews
+from .serializers import ReviewsSerializer
 
 # Create your views here.
-def index(req):
-    return render(req, 'index.html')
+class ReviewsView(ModelViewSet):
+    http_method_names = ['get']
+    queryset = Reviews.objects.all()
+    serializer_class = ReviewsSerializer
 
