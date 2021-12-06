70089269db872b5e7989826eff1f7b4d083cf62b
olu-akano <akanopaul@yahoo.co.uk>
2021-12-06 16:48:35 +0000

    registe review models to admin

--- server/travelado/reviews/urls.py
+++ server/travelado/reviews/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ListReviewsView.as_view(), name='index'),
-    path("create", views.CreateReviewsView.as_view(),name="create_review"),
+    path("create/", views.CreateReviewsView.as_view(),name="create_review"),
     path("update/<int:pk>/",views.UpdateReviewsView.as_view(),name="update_review"),
     path("delete/<int:pk>/",views.DeleteReviewsView.as_view(),name="delete_review")
 ]
