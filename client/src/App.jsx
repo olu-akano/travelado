01c2c5b8f69598ed97dff96444f486cae52b8224
saja-aabith <saja.aabith@gmail.com>
2021-12-06 15:37:35 +0000

    removing custompopup func

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -41,24 +41,6 @@ export const App = () => {
 
     const [markers, setMarkers] = useState([]);
 
-    const CustomPopup = ({ index, marker, closePopup }) => {
-        return (
-            <Popup
-                latitude={marker.latitude}
-                longitude={marker.longitude}
-                onClose={closePopup}
-                closeButton={true}
-                closeOnClick={false}
-                offsetTop={-30}
-            >
-                <p>{marker.name}</p>
-            </Popup>
-        )
-    };
-
-
-
-
 
     const handleChange = (event) => {
         setAuth(event.target.checked);
