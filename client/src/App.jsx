f2cdb068da9d408c6a869525a6d8e3ebec22aac0
saja-aabith <saja.aabith@gmail.com>
2021-12-07 01:27:39 +0000

    merge confilcts resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -51,9 +51,10 @@ export const App = () => {
     const [titleField, setTitleField] = useState("");
     const [commentField, setCommentField] = useState("");
     const [ratingField, setRatingField] = useState(0);
+    const [formError, setFormError] = useState(false);
+
     const [currentUser, setCurrentUser] = useState("Saja");
     const mapRef = useRef();
-
     const [auth, setAuth] = useState(true);
     const [anchorEl, setAnchorEl] = useState(null);
     const [viewport, setViewport] = useState(
@@ -64,7 +65,8 @@ export const App = () => {
         });
 
     const [markers, setMarkers] = useState([]);
-
+    const [selectedMarker, setSelectedMarker] = useState({});
+    const [showPopup, setShowPopup] = useState(false);
 
     const handleViewportChange = useCallback(
         (newViewport) => setViewport(newViewport),
@@ -83,28 +85,7 @@ export const App = () => {
     );
 
 
-    const CustomPopup = ({ index, marker, closePopup }) => {
-        return (
-            <Popup
-                latitude={marker.latitude}
-                longitude={marker.longitude}
-                onClose={closePopup}
-                closeButton={true}
-                closeOnClick={false}
-                offsetTop={-30}
-            >
-                <p>{marker.name}</p>
-            </Popup>
-        )
-    };
-
-    useEffect(() => {
-
-    }, [])
-
     // events
-
-
     const handleChange = (event) => {
         setAuth(event.target.checked);
     };
@@ -122,90 +103,76 @@ export const App = () => {
         setDialogOpen(false);
     }
 
-
+    const resetForm = () => {
+        setTitleField("")
+        setCommentField("")
+        setRatingField(0)
+        setFormError(false)
+    };
 
 
     const showDialog = (clickedLocation) => {
         let lat = clickedLocation.lngLat[1]
         let lng = clickedLocation.lngLat[0]
 
-        setcurrentClickedMarkerLatLng([lat, lng])
-        setDialogOpen(true)
+        if (auth) {
+            setcurrentClickedMarkerLatLng([lat, lng])
+            setDialogOpen(true)
+        }
     }
 
 
+    const addMarker = () => {
+        const lat = currentClickedMarkerLatLng[0]
+        const lng = currentClickedMarkerLatLng[1]
+        if (titleField !== "" && commentField !== "" && ratingField !== 0) {
+            setMarkers([
+                ...markers,
+                {
+                    latitude: lat,
+                    longitude: lng,
+                    title: titleField,
+                    comment: commentField,
+                    rating: ratingField,
+                    userName: currentUser
+                }
+            ])
 
-    const onSelected = (item) => {
-        setMarker({
-            name: item.place_name,
-            longitude: item.center[0],
-            latitude: item.center[1]
-        })
+            setDialogOpen(false)
+            resetForm()
+        } else {
+            setFormError(true)
+        }
     }
+
+    const handleMarkerClick = (marker) => {
+        setSelectedMarker(marker)
+        setShowPopup(true)
+
+    }
+
 
     const markerCollection = [];
     markers.forEach((marker, index) => {
         markerCollection.push(
             <Marker
+                onClick={() => { handleMarkerClick(marker) }}
                 key={index}
                 longitude={marker.longitude}
-                latitude={marker.latitude}>
+                latitude={marker.latitude}
+            >
                 <div className="marker temporary-marker"><span></span></div>
             </Marker>
         )
     })
 
 
-
-
-
-
-    const addMarker = () => {
-        const lat = currentClickedMarkerLatLng[0]
-        const lng = currentClickedMarkerLatLng[1]
-
-        setMarkers([
-            ...markers,
-            {
-                latitude: lat,
-                longitude: lng,
-                title: titleField,
-                comment: commentField,
-                rating: ratingField,
-                userName: currentUser
-            }
-        ])
+    const formErrorDiv = () => {
+        if (formError) {
+            return <div style={{ 'color': 'red' }}><strong>Please complete all fields</strong></div>
+        }
     }
 
-    const markerCollection = [];
-    markers.forEach((marker, index) => {
-        markerCollection.push(
-            <div key={index}>
-
-                <Marker
-                    longitude={marker.longitude}
-                    latitude={marker.latitude} >
-                    <div className="marker temporary-marker"><span></span></div>
-                </Marker >
-                <Popup
-                    latitude={marker.latitude}
-                    longitude={marker.longitude}
-                    closeButton={true}
-                    closeOnClick={false}
-                    anchor="top">
-                    <div>{marker.title}</div>
-                    <div>{marker.userName}</div>
-                    <div>{marker.comment}</div>
-                    <div>{marker.rating}</div>
-
-                </Popup>
-
-            </div >
-
-
-
-        )
-    })
 
     return (
         <>
@@ -281,29 +248,69 @@ export const App = () => {
                 onViewportChange={(viewport) => setViewport(viewport)}
                 onClick={(clickedLocation) => showDialog(clickedLocation)}
             >
-
                 <SearchForm mapRef={mapRef} mapboxApiKey={mapboxApiKey} viewport={viewport} handleGeocoderViewportChange={handleGeocoderViewportChange} />
                 <GeoLocater />
 
                 {markerCollection}
+                {showPopup && (<Popup
+                    latitude={selectedMarker.latitude}
+                    longitude={selectedMarker.longitude}
+                    closeButton={true}
+                    closeOnClick={false}
+                    onClose={() => setShowPopup(false)}
+                    anchor="top" >
+                    <div>{selectedMarker.title}</div>
+                    <div>{selectedMarker.comment}</div>
+                    <div>{selectedMarker.rating}/5</div>
+                    <div>{selectedMarker.userName}</div>
+                    {/* <div>{marker.timestamp}</div> */}
+                </Popup>)}
+
             </ReactMapGL>
-            <Dialog open={dialogOpen}>
-                <DialogTitle>Trevalago marker</DialogTitle>
+            <Dialog open={dialogOpen} onClose={handleDialogClose}>
+                <DialogTitle>Trevalado Marker</DialogTitle>
                 <DialogContent>
                     <DialogContentText>
                         Add your comments here!
                     </DialogContentText>
                     <TextField
-                        requried
                         value={titleField}
                         onChange={(e) => setTitleField(e.target.value)}
+                        autoFocus
+                        required
                         margin="dense"
                         id="title"
+                        label="Title"
                         helperText="Give your comment a title"
                         type="text"
                         fullWidth
-                        variant='outlined'
+                        variant="outlined"
                     />
+                    <TextField
+                        value={commentField}
+                        onChange={(e) => setCommentField(e.target.value)}
+                        required
+                        margin="dense"
+                        id="comment"
+                        label="Comment"
+                        multiline
+                        minRows="3"
+                        type="text"
+                        fullWidth
+                        variant="outlined"
+                    />
+                    <br></br>
+                    <br></br>
+                    <Rating
+                        value={ratingField}
+                        onChange={(e) => setRatingField(Number(e.target.value))}
+                        name="rating"
+                        precision={0.5}
+                        size="large"
+                    />
+                    <br></br>
+                    <br></br>
+                    {formErrorDiv()}
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleDialogClose}>Cancel</Button>
