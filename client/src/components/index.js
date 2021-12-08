5c9241b970ba7e77fd815ac9af95d1bd93c33203
saja-aabith <saja.aabith@gmail.com>
2021-12-08 01:59:52 +0000

    remove webscraper from index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,4 @@
 export { SearchForm } from './SearchForm'
 export { GeoLocater } from './GeoLocater'
 export { RegisterOrLogin } from './RegisterOrLogin'
-export { WebScraper } from './WebScraper'
 export { CovidData } from './CovidData'
