f55915181d1424b74cd6008ea50c15b75d4cebf0
olu-akano <akanopaul@yahoo.co.uk>
2021-12-08 16:59:06 +0000

    remove client folder

--- client/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { SearchForm } from './SearchForm'
-export { GeoLocater } from './GeoLocater'
-export { RegisterOrLogin } from './RegisterOrLogin'
-export { CovidData } from './CovidData'
-export { CountrySelect } from './CountrySelect'
