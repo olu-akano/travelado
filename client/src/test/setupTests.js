679eb4f9694e70ca7e523b88d217656fbc0f2a3f
saja-aabith <saja.aabith@gmail.com>
2021-12-09 08:58:59 +0000

    add page tests

--- client/src/test/setupTests.js
+++ client/src/test/setupTests.js
@@ -1,3 +1,9 @@
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-17";
-configure({ adapter: new Adapter() });
+import React from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
+
+
+global.React = React;
+global.render = render;
+global.userEvent = userEvent;
