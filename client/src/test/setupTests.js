9e2cd430c24b59ffa0e2b30f7f396fd02251eb50
saja-aabith <saja.aabith@gmail.com>
2021-12-09 01:46:50 +0000

    starting testing correctly

--- client/src/test/setupTests.js
+++ client/src/test/setupTests.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import '@testing-library/jest-dom/extend-expect';
-global.React = React;
-global.render = render;
-global.userEvent = userEvent;
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-17";
+configure({ adapter: new Adapter() });
